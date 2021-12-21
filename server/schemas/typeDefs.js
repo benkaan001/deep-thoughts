bf8c6018d475f136ac3ed664d9a4ea090240db20
Ben Kaan <benkaan001@gmail.com>
2021-12-21 16:17:37 -0600

    add user Auth with JWT

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,23 @@
 // the qql tagged template function makes the tagged templates a bit more understandable and explicit
 
+// both mutations will return a User object - the User who either has successfully
+// logged in or twho was just created on signup. --> ! indicates a required argument.
+
+// --> This means that an Auth type must return a token and can optionally include any
+//other user data. Update the two mutations to return an Auth object instead of a User object.
+
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+
   type Thought {
     _id: ID
     thoughtText: String
@@ -18,19 +33,26 @@ const typeDefs = gql`
     createdAt: String
     username: String
   }
-  type User {
-    _id: ID
-    username: String
-    email: String
-    freiendCount: Int
-    thoughts: [Thought]
-    friends: [User]
+
+  type Auth {
+    token: ID!
+    user: User
   }
+
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
   }
 `;
 
---
+++ server/utils/auth.js
@@ -0,0 +1,44 @@
+const jwt = require('jsonwebtoken');
+
+const secret = 'clearly not a secret';
+const expiration = '2h';
+
+const signToken = (user) => {
+  const { username, email, _id } = user;
+  const payload = { username, email, _id };
+
+  return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+};
+
+// module.exports = {
+//   signToken: function ({ username, email, _id }) {
+//     const payload = { username, email, _id };
+
+//     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+//   },
+// };
+
+const authMiddleware = ({ req }) => {
+  let token = req.body.token || req.query.token || req.headers.authorization;
+
+  if (req.headers.authorization) {
+    token = token.split(' ').pop().trim();
+  }
+
+  if (!token) {
+    return req;
+  }
+
+  // wrap the verify() method in a try...catch statement to mute the error. We will manually
+  // throw an authentication error on the resolver side
+  try {
+    const { data } = jwt.verify(token, secret, { maxAge: expiration });
+    req.user = data;
+  } catch {
+    console.log('Invalid token!');
+  }
+
+  return req;
+};
+
+module.exports = { signToken, authMiddleware };
