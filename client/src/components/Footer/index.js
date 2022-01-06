809e46d24e8d9f3681fe8de8ede455cbd90cf052
benkaan001 <benkaan001@gmail.com>
2022-01-06 12:45:32 -0600

    remove seeds file & resolve error in ReactionList component

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='w-100 mt-auto bg-secondary p-4'>
       <div className='container'>
-        &copy;{new Date().getFullYear()} Made with love
+        &copy;{new Date().getFullYear()} Made with Love
       </div>
     </footer>
   );
